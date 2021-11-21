702eebce61c4c58291b8c66b6e1e04a202e3df14
Chol Nhial <chol.nhial.chol@gmail.com>
2021-11-21 11:13:32 +1000

    spring

---
+++ src/main/java/com/cholnhial/taca/TacaApplication.java
@@ -0,0 +1,13 @@
+package com.cholnhial.taca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cholnhial/taca/TacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cholnhial.taca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
