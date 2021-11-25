370637d2fdc2c8fcdcce0463314c319d6b4971ae
Chol Nhial <chol.nhial.chol@gmail.com>
2021-11-26 08:16:52 +1000

    change id generation strategy to identity for auto-increment

--- src/main/java/com/cholnhial/taca/domain/Room.java
+++ src/main/java/com/cholnhial/taca/domain/Room.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Room {
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
--- src/main/java/com/cholnhial/taca/domain/User.java
+++ src/main/java/com/cholnhial/taca/domain/User.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
