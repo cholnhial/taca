d0b6c19439fa9187eb958b02ad6db6f116e87678
Chol Nhial <chol.nhial.chol@gmail.com>
2021-12-04 22:07:32 +1000

    scroll to the bottom when a new message is added to props.messages

--- src/frontend/src/components/Chat/Chat.jsx
+++ src/frontend/src/components/Chat/Chat.jsx
@@ -1,6 +1,6 @@
 import {Button, Card, Col, FloatingLabel, Form, Row} from "react-bootstrap";
 import styles from './Chat.module.css'
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import MessageTone from "../MessageTone/MessageTone";
 import OtherUser from "../OtherUser/OtherUser";
 import Message from "../Message/Message";
@@ -8,6 +8,11 @@ import './Chat.css';
 
 const Chat = (props) => {
     const [message, setMessage] = useState('')
+    const messagesContainer  = useRef(null);
+
+    useEffect(() => {
+        scrollToBottomOfMessagesContainer();
+    }, [props.messages])
 
     const handleMessageInputChange = (event) => {
         setMessage(event.target.value);
@@ -19,13 +24,23 @@ const Chat = (props) => {
         setMessage('');
     }
 
+    /* Copied from https://codesandbox.io/s/8l2y0o24x9?file=/src/index.js
+    *  modified to use useRef()
+    * */
+    const scrollToBottomOfMessagesContainer = () => {
+        const scroll =
+            messagesContainer.current.scrollHeight -
+            messagesContainer.current.clientHeight;
+        messagesContainer.current.scrollTo(0, scroll);
+    };
+
     return (
         <Card className={[styles.chat, "mx-auto", "border-0"]} style={{opacity: 0.8}}>
             <Card.Header className={[styles['card-header']]}><span className="fs-3 fw-bolder">CHAT</span>
                 <hr />
                 <OtherUser tone={props.otherUserMessageTone} username={props.otherUsername}/>
             </Card.Header>
-            <Card.Body className="text-center overflow-scroll" style={{height: "15rem"}}>
+            <Card.Body ref={messagesContainer} className="text-center overflow-scroll" style={{height: "15rem"}}>
                 {
                     props.messages.map((m, i) => {
                         return <Message key={i} username={props.username} last={props.messages.length == i+1} message={m} />
